46cde5c0957b98b6701fd3c6547269d215800ebc
alan laiju <alanlaiju1212@gmail.com>
2026-06-20 14:12:17 +0530

    second commit

--- wasp/views.py
+++ wasp/views.py
@@ -1,28 +1,39 @@
-from django.shortcuts import render,redirect
-from . models import todo
+from django.shortcuts import render, redirect, get_object_or_404
+from .models import todo
+
+
 def index(request):
-  item=todo.objects.all()
-  if request.method=='POST':
-    a=request.POST.get('task-name')
-    b=request.POST.get('task-date')
-    todo.objects.create(taskname=a,taskdate=b)
+    if request.method == 'POST':
+        task_name = request.POST.get('task-name')
+        task_date = request.POST.get('task-date')
+
+        todo.objects.create(
+            taskname=task_name,
+            taskdate=task_date
+        )
+        return redirect('home')
+
+    item = todo.objects.all()
+    return render(request, 'index.html', {'item': item})
+
+
+def deletetask(request, id):
+    task = get_object_or_404(todo, id=id)
+
+    if request.method == 'POST':
+        task.delete()
+        return redirect('home')
+
     return redirect('home')
-  return render(request, 'index.html',{'item':item})
 
 
-def deletetask(request,id):
-  a=todo.objects.filter(id=id)
-  a.delete()
-  return redirect('home')
+def updatetask(request, id):
+    task = get_object_or_404(todo, id=id)
 
+    if request.method == 'POST':
+        task.taskname = request.POST.get('task-name')
+        task.taskdate = request.POST.get('task-date')
+        task.save()
+        return redirect('home')
 
-def updatetask(request,id):
-    a=todo.objects.get(id=id)
-    if request.method=='POST':
-     a.taskname=request.POST.get('task-name')
-     a.taskdate=request.POST.get('task-date')
-     a.save()
-     return redirect('home')
-    return render(request, 'update.html', {'a':a})
-
-# Create your views here.
+    return render(request, 'update.html', {'a': task})
